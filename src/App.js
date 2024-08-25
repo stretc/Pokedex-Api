2631de7dd372a2ba1f702a273dcb3c4d7e44c79d
Caleb <calebnull88@outlook.com>
2024-08-25 11:40:11 -0500

    Completed the basic app along with some transitions.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import colors from './assets/TypeColors';
+import { colors, styles } from './assets/TypeStyles';
 import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 import pokeballSvg from './assets/Pokeball/2.svg';
-import RadarChart from './Components/RadarChart';
+import { IconReact, IconNodejs, IconExpress } from './assets/svgPaths';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -20,18 +18,19 @@ function App() {
   const [number, setNumber] = useState('Searching...');
   const [name, setName] = useState('Searching...');
   const [total, setTotal] = useState('Searching...');
-  const [type, setType] = useState('Searching...');
+  const [types, setTypes] = useState([]);
   const [attack, setAttack] = useState('Searching...');
   const [defense, setDefense] = useState('Searching...');
   const [spAttack, setSpAttack] = useState('Searching...');
   const [spDefense, setSpDefense] = useState('Searching...');
   const [speed, setSpeed] = useState('Searching...');
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:5000/api/pokedex')
       .then((response) => response.json())
       .then((data) => {
-        setPokemonList(data); // Store the entire Pokémon list
+        setPokemonList(data);
       })
       .catch((error) => console.error('Error fetching the Pokémon data:', error));
   }, []);
@@ -52,114 +51,128 @@ function App() {
         setNumber(foundPokemon.number || 'Searching...');
         setName(foundPokemon.name || 'Searching...');
         setTotal(foundPokemon.total || 'Searching...');
-        setType(foundPokemon.type || 'Searching...');
+        const pokemonTypes = foundPokemon.type ? foundPokemon.type.split(',').map(type => type.trim()) : [];
+        setTypes(pokemonTypes);
         setAttack(foundPokemon.attack || 'Searching...');
         setDefense(foundPokemon.defense || 'Searching...');
         setSpAttack(foundPokemon.spAttack || 'Searching...');
         setSpDefense(foundPokemon.spDefense || 'Searching...');
         setSpeed(foundPokemon.speed || 'Searching...');
         setPokemonData(foundPokemon);
+        setIsExpanded(true); // Expand the content when data is found
       } else {
-        // Reset fields if no Pokémon is found
         setImage(pokeballSvg);
         setNumber('Searching...');
         setName('Searching...');
         setTotal('Searching...');
-        setType('Searching...');
+        setTypes([]);
         setAttack('Searching...');
         setDefense('Searching...');
         setSpAttack('Searching...');
         setSpDefense('Searching...');
         setSpeed('Searching...');
         setPokemonData(null);
+        setIsExpanded(false); // Collapse the content when no data is found
       }
       setError('Nothing found!');
     } catch (error) {
       setError(error.message);
       setPokemonData(null);
-
-      // Reset fields on error
       setImage(pokeballSvg);
       setNumber('Searching...');
       setName('Searching...');
       setTotal('Searching...');
-      setType('Searching...');
+      setTypes([]);
       setAttack('Searching...');
       setDefense('Searching...');
       setSpAttack('Searching...');
       setSpDefense('Searching...');
       setSpeed('Searching...');
+      setIsExpanded(false); // Collapse the content on error
     }
   };
 
+  const typeColors = types.map(type => colors[type.toLowerCase()] || 'black');
+
   return (
     <div className="App">
-      <section id="main-section grid-container">
-        <header className="App-header grid-container">
-          <h1>Picodex</h1>
-          <nav className="main-nav grid-container">
-            <ul className="main-nav-list">
-              <li className="item"><a href="#">Tools</a></li>
-              <li className="item"><a href="#">Resources</a></li>
-            </ul>
-          </nav>
-        </header>
-        <main id="main" className="main-content grid-container">
-          <div className="grid-row">
-            <div className="grid-col">
-              <div className="filters">
-                <Box sx={{ padding: '16px' }}>
-                  <label htmlFor="filter-name">Name:</label>
-                  <Stack spacing={2} sx={{ width: 300 }}>
-                    <Autocomplete
-                      freeSolo
-                      id="autocomplete"
-                      disableClearable
-                      options={pokemonList.map((option) => option.name)}
-                      onInputChange={handleChange}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          InputProps={{
-                            ...params.InputProps,
-                            type: 'search',
-                            sx: {
-                              height: '40px',
-                            },
-                          }}
-                        />
-                      )}
-                    />
-                  </Stack>
-                  <button className='submit-button' onClick={handleSubmit}>
-                    Submit
-                  </button>
-                </Box>
+      <header className="App-header grid-container">
+        <h1>Picodex</h1>
+      </header>
+      <main id="main" className={`main-content grid-container ${isExpanded ? 'expanded' : ''}`}>
+        <div className="grid-row">
+          <div className="grid-col">
+            <div className="filters">
+              <Box sx={{ padding: '16px' }}>
+                <label htmlFor="filter-name">Name:</label>
+                <Stack spacing={2} sx={{ width: 300 }}>
+                  <Autocomplete
+                    freeSolo
+                    id="autocomplete"
+                    disableClearable
+                    options={pokemonList.map((option) => option.name)}
+                    onInputChange={handleChange}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        InputProps={{
+                          ...params.InputProps,
+                          type: 'search',
+                          sx: {
+                            height: '40px',
+                          },
+                        }}
+                      />
+                    )}
+                  />
+                </Stack>
+                <button className='submit-button' onClick={handleSubmit}>
+                  Submit
+                </button>
+              </Box>
+            </div>
+            <div className='pokemon-chosen'>
+              <div className='pokemon-chosen-image'>
+                <img src={image} alt={pokeballSvg} className='poke-image' />
               </div>
-              <div className='pokemon-chosen'>
-                <div className='pokemon-chosen-image'>
-                  <img src={image} alt={pokeballSvg} className='poke-image inactive' />
+              <div className='poke-stats'>
+                <p style={styles.number} className='number'># {number}</p>
+                <p style={styles.name} className='name'>{name}</p>
+                <div className='pokemon-types'>
+                  {types.map((type, index) => (
+                    <p key={index} className='type' style={{
+                      backgroundColor: typeColors[index],
+                      color: '#fff',
+                    }}>
+                      {type}
+                    </p>
+                  ))}
                 </div>
-                <div className='poke-stats'>
-                  <p className='number'># {number}</p>
-                  <p className='name'>{name}</p>
-                  <p className='type'>{type}</p>
-                  <p className='attack'>Attack: {attack}</p>
-                  <p className='defense'>Defense: {defense}</p>
-                  <p className='sp-attack'>
-                    Sp. Attack: {spAttack}
-                  </p>
-                  <p className='sp-defense'>
-                    Sp. Defense: {spDefense}
-                  </p>
-                  <p className='speed'>{speed}</p>
-                  <p className='total'>{total}</p>
-                </div>
+                <p style={styles.attack} className='attack'>Attack: {attack}</p>
+                <p style={styles.defense} className='defense'>Defense: {defense}</p>
+                <p style={styles.spAttack} className='sp-attack'>
+                  Sp. Attack: {spAttack}
+                </p>
+                <p style={styles.spDefense} className='sp-defense'>
+                  Sp. Defense: {spDefense}
+                </p>
+                <p style={styles.speed} className='speed'>Speed: {speed}</p>
+              </div>
+              <div className='total-div'>
+                <p style={styles.total} className='total'>Total: {total}</p>
               </div>
             </div>
           </div>
-        </main>
-      </section>
+        </div>
+      </main>
+      <footer>
+        <div className='footer-div'>
+          <IconReact />
+          <IconNodejs />
+          <IconExpress />
+          <a href='https://pokemondb.net/pokedex/all' target='_blank'>Resource</a>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/assets/TypeStyles.js
@@ -0,0 +1,52 @@
+const colors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+};
+
+const styles = {
+  number: {
+    fontWeight: 'bold',
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  type: {
+    fontWeight: 'bold',
+  },
+  total: {
+    fontWeight: 'bold',
+  },
+  attack: {
+    fontWeight: 'bold',
+  },
+  defense: {
+    fontWeight: 'bold',
+  },
+  spAttack: {
+    fontWeight: 'bold',
+  },
+  spDefense: {
+    fontWeight: 'bold',
+  },
+  speed: {
+    fontWeight: 'bold',
+  },
+}
+
+export { colors, styles };
